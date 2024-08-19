71e12d2923d4439de692179907e1378178196bdd
Nishant <rajputnishant393@gmail.com>
2024-08-19 09:59:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById("background-video");
+    video.playbackRate = 0.5; // Slow down the video to 50% speed
+});
